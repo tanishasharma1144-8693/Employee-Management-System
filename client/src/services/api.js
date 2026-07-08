d86b940eeea5578f5ae4974c13b78bd054747878
tanishasharma1144-8693 <tanishasharma1144@gmail.com>
2026-07-08 07:30:42 +0530

    Connect frontend with deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "http://localhost:5000/api",
+  baseURL: "https://employee-management-system-z0mi.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
